0667632f3953f9ebdd28a9a1f6720e98ad89e005
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2024-07-07 16:39:28 -0300

    Iniciando a Implementação do Repositório de Produtos

---
+++ net-ms/GeekShopping/GeekShopping.ProductAPI/Repository/ProductRepository.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace GeekShopping.ProductAPI.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+
+        private readonly MySQLContext _context;
+
+        private IMapper _mapper;
+
+        public ProductRepository(MySQLContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public Task<ProductVO> Create(ProductVO vo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductVO> Update(ProductVO vo)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<bool> Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<ProductVO>> FindAll()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            return _mapper.Map<List<ProductVO>>(products);
+        }
+
+        public async Task<ProductVO> FindById(long id)
+        {
+            var product = await _context.Products.Where(p => p.Id == id).FirstOrDefaultAsync();
+
+            return _mapper.Map<ProductVO>(product);
+        }
+
+    }
+}
