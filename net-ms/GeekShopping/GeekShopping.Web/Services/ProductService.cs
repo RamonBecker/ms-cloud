82553eb9fd25e65a38e2c72f0dc00e66beb652ee
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2024-07-10 09:53:31 -0300

    Implementando a Classe ProductService

---
+++ net-ms/GeekShopping/GeekShopping.Web/Services/ProductService.cs
@@ -0,0 +1,34 @@
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.IServices;
+using GeekShopping.Web.Utils;
+
+namespace GeekShopping.Web.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _cliente;
+        public const string BasePath = "api/v1/product";
+        public async Task<IEnumerable<ProductModel>> FindAllProducts()
+        {
+            var response = await _cliente.GetAsync(BasePath);
+            return await response.ReadContentAs<List<ProductModel>>();
+        }
+        public async Task<ProductModel> FindProductById(long id)
+        {
+            var response = await _cliente.GetAsync($"{BasePath}/{id}");
+            return await response.ReadContentAs<ProductModel>();
+        }
+        public Task<ProductModel> CreateProduct(ProductModel model)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ProductModel> UpdateProduct(ProductModel model)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<bool> DeleteProduct(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
