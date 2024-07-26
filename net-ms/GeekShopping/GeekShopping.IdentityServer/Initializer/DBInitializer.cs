0372ec1c8c4959316ce0ebae352c4d7397f85244
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2024-07-26 11:03:05 -0300

    Criando a Classe Responsável por Inicializar o Banco de Dados

---
+++ net-ms/GeekShopping/GeekShopping.IdentityServer/Initializer/DBInitializer.cs
@@ -0,0 +1,32 @@
+﻿using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Model;
+using GeekShopping.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Identity;
+
+namespace GeekShopping.IdentityServer.Initializer
+{
+    public class DBInitializer : IDBInitializer
+    {
+        private readonly MySQLContext _context;
+        private readonly UserManager<ApplicationUser> _user;
+        private readonly RoleManager<ApplicationUser> _role;
+
+        public DBInitializer(MySQLContext context, 
+                             UserManager<ApplicationUser> user, 
+                             RoleManager<ApplicationUser> role)
+        {
+            _context = context;
+            _user = user;
+            _role = role;
+        }
+
+        public void Initializer()
+        {
+            if (_role.FindByNameAsync(IdentityConfiguration.Admin).Result != null)
+                return;
+
+            _role.CreateAsync(new IdentityRole(IdentityConfiguration.Admin)).GetAwaiter().GetResult();
+            _role.CreateAsync(new IdentityRole(IdentityConfiguration.Client)).GetAwaiter().GetResult();
+        }
+    }
+}
