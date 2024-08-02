395d3acafca4e4c35b89ecc394aa13a968767d74
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2024-08-02 08:14:19 -0300

    Setando o JWT Token no Header das Operações

--- net-ms/GeekShopping/GeekShopping.IdentityServer/Services/ProfileService.cs
+++ net-ms/GeekShopping/GeekShopping.IdentityServer/Services/ProfileService.cs
@@ -4,11 +4,8 @@ using Duende.IdentityServer.Services;
 using GeekShopping.IdentityServer.Model;
 using IdentityModel;
 using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
-using System.Threading.Tasks;
+
 
 namespace GeekShopping.IdentityServer.Services
 {
