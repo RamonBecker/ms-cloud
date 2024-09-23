9e240efb347482acfda3589d151f0d83827e2f40
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2024-09-23 09:19:16 -0300

    Implementando a Tela de Carrinho de Compras - Body

---
+++ net-ms/GeekShopping/GeekShopping.IdentityServer/Migrations/20240923115714_AddUpdateIdentityServerBD.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeekShopping.IdentityServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUpdateIdentityServerBD : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
