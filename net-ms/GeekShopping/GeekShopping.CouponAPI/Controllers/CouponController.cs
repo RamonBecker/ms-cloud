07625b87572d85e5291ab66497000a274dfc023f
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2024-10-03 11:57:59 -0300

    Implementando o CouponController

---
+++ net-ms/GeekShopping/GeekShopping.CouponAPI/Controllers/CouponController.cs
@@ -0,0 +1,32 @@
+﻿using GeekShopping.CouponAPI.Data.ValueObjects;
+using GeekShopping.CouponAPI.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.CouponAPI.Controllers
+{
+
+	[ApiController]
+	[Route("api/v1/[controller]")]
+	public class CouponController : ControllerBase
+	{
+		private ICouponRepository _repository;
+
+		public CouponController(ICouponRepository repository)
+		{
+			_repository = repository ?? throw new ArgumentNullException(nameof(repository));
+		}
+
+		[Authorize]
+		[HttpGet("{code}")]
+		public async Task<ActionResult<CouponVO>> FindByCode(string code)
+		{
+			var coupon = await _repository.GetCouponByCode(code);
+
+			if (coupon == null)
+				return NotFound();
+
+			return Ok(coupon);
+		}
+	}
+}
