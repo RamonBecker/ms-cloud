fc3b01d20c9e5418051ac94af1ded83b17cd34d0
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2024-10-29 11:27:42 -0300

    Ajustando a Autenticação, o Swagger (OpenAPI) e as Mensagens

--- net-ms/GeekShopping/GeekShopping.OrderAPI/Program.cs
+++ net-ms/GeekShopping/GeekShopping.OrderAPI/Program.cs
@@ -56,7 +56,6 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.OrderAPI", Version = "v1" });
-    c.EnableAnnotations();
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
         Description = @"Enter 'Bearer' [space] and your token!",
